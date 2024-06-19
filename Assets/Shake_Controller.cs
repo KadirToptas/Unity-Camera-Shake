54a02c7e548afec1d0b3490c7e47cb49587acab1
KadirToptas <kdrrtptss@gmail.com>
2024-06-19 19:26:43 +0300

    Camera-Shake-2D

--- Assets/Shake_Controller.cs
+++ Assets/Shake_Controller.cs
@@ -1,31 +1,37 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Shake_Controller : MonoBehaviour
 {
-    [SerializeField] private Transform cam;
+    [SerializeField] private Transform _cam;
+    [SerializeField] private Vector3 _startPos;
     
-    [SerializeField] private Vector3 startPos;
-
+    [SerializeField] private float _shakePower;
+    [SerializeField] private float _shakeDuration;
     private float _initialDuration;
+    [SerializeField] private float _downAmount;
+    [SerializeField] private bool _isShake = false;
 
-    [SerializeField] private float shakePower;
-    [SerializeField] private float shakeDuration;
-    [SerializeField] private float downAmount;
-
-    [SerializeField] private bool isShaking = false;
-    
     void Start()
     {
-        cam = Camera.main.transform;
+        _cam = Camera.main.transform;
+        _startPos = _cam.localPosition;
+        _initialDuration = _shakeDuration;
     }
-    
-    
 
     void Update()
     {
-        
+        if(_isShake) {
+            if(_shakeDuration > 0) {
+                _cam.localPosition = _startPos + Random.insideUnitSphere * _shakePower  ;
+                _shakeDuration -= _downAmount * Time.deltaTime;
+            }
+            else {
+                _isShake = false;
+                _cam.localPosition = _startPos;
+                _shakeDuration = _initialDuration;
+            }
+        }
     }
 }
